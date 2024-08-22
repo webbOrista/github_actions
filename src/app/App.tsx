c989c7a19acdaabbede99250211f6bb36ad6110f
Boris Butorin <140898204+webbOrista@users.noreply.github.com>
2024-08-22 22:09:35 +0300

    Analytics events added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import styles from './app.module.scss';
+import {useMetrika} from ../hooks/useMetrika;
 const Modal = React.lazy(
 	() => import('../components/modal' /*webpackChunkName: "modal" */)
 );
@@ -13,9 +14,22 @@ const AnotherComponent = React.lazy(
 );
 
 export const App = () => {
+	const { ym, gtag } = useMetrika();
 	return (
-		<div className={styles.page}>
-			<div className={styles.links}>
+		 <div className={styles.page}>
+            <button className={styles.link} onClick={() => ym('reachGoal', 'buy')}>
+                Купить
+            </button>
+            <button
+                className={styles.link}
+                onClick={() =>
+                    gtag('event', 'add_to_cart', {
+                        event_name: 'add_to_cart',
+                    })
+                }>
+                Купить в 1 клик
+            </button>
+ 			<div className={styles.links}>
 				<Link to='/another' className={styles.link}>
 					Перейти на станицу c компонентом AnotherComponent
 				</Link>
